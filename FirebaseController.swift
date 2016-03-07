41303a2953def1c7200eb01804a58491c1af3d86
Diego-F-Aguirre <diego3884@gmail.com>
2016-03-06 23:07:26 -0700

    FirebaseController setup
    data endpoints and observe data endpoints setup

--- FirebaseController.swift
+++ FirebaseController.swift
@@ -7,19 +7,93 @@
 //
 
 import UIKit
-import FBSDKCoreKit
-import FBSDKLoginKit
 import Firebase
 
 class FirebaseController {
     
     static let base = Firebase(url: "https://cliqueus.firebaseio.com")
     
-
-//    func authenticateUser() {
+    static func dataAtEndpoint(endpoint: String, completion: (data: AnyObject?) -> Void) {
+        
+        let baseForEndpoint = FirebaseController.base.childByAppendingPath(endpoint)
+        
+        baseForEndpoint.observeSingleEventOfType(.Value, withBlock: { snapshot in
+            
+            if snapshot.value is NSNull {
+                completion(data: nil)
+            } else {
+                completion(data: snapshot.value)
+            }
+            
+        })
+    }
     
-//        let ref = Firebase(url: "https://<YOUR-FIREBASE-APP>.firebaseio.com")
-       
-
+    
+    static func observeDataAtEndpoint(endpoint: String, completion: (data: AnyObject?) -> Void) {
+        
+        let baseForEndpoint =  FirebaseController.base.childByAppendingPath(endpoint)
+        
+        baseForEndpoint.observeEventType(.Value, withBlock: { snapshot in
+            
+            if snapshot.value is NSNull {
+                completion(data: nil)
+            } else {
+                completion(data: snapshot.value)
+            }
+        })
+    }
     
 }
+
+protocol FireBaseType {
+    var identifier: String? { get set }
+    var endpoint: String { get }
+    var jsonValue: [String: AnyObject] { get }
+    
+    init?(json: [String: AnyObject], identifier: String)
+    
+    mutating func save()
+    func delete()
+}
+
+extension FireBaseType {
+    
+    mutating func save() {
+        
+        var endpointBase: Firebase
+        
+        if let identifier = self.identifier {
+            endpointBase = FirebaseController.base.childByAppendingPath(endpoint).childByAppendingPath(identifier)
+        } else {
+            endpointBase = FirebaseController.base.childByAppendingPath(endpoint).childByAutoId()
+            self.identifier = endpointBase.key
+        }
+        endpointBase.updateChildValues(self.jsonValue)
+    }
+    
+    
+    func delete() {
+        
+        if let identifier = self.identifier {
+            let endpointBase: Firebase = FirebaseController.base.childByAppendingPath(endpoint).childByAppendingPath(identifier)
+            
+            endpointBase.removeValue()
+        }
+    }
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
