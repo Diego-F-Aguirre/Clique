7f8943702930506f323f4a80450515fcd669e7ae
Diego-F-Aguirre <diego3884@gmail.com>
2016-03-06 22:39:12 -0700

    Facebook login authentication setup
    Still need to connect it to a user id on the Firebase side.
    
    Also changed status bar to white in the LoginViewController

--- FirebaseController.swift
+++ FirebaseController.swift
@@ -7,11 +7,19 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
 import Firebase
 
 class FirebaseController {
- 
+    
     static let base = Firebase(url: "https://cliqueus.firebaseio.com")
     
+
+//    func authenticateUser() {
+    
+//        let ref = Firebase(url: "https://<YOUR-FIREBASE-APP>.firebaseio.com")
+       
+
     
 }
--- LoginViewController.swift
+++ LoginViewController.swift
@@ -7,31 +7,57 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 class LoginViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-   
-    @IBAction func loginButtonPressed(sender: AnyObject) {
-        
-        var ref = fireb
         
     }
     
-
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        
+        return UIStatusBarStyle.LightContent
+    }
+    
+    
+    @IBAction func loginButtonPressed(sender: AnyObject) {
+        
+        let facebookLogin = FBSDKLoginManager()
+        facebookLogin.logInWithReadPermissions(["email"], handler: {
+            (facebookResult, facebookError) -> Void in
+            if facebookError != nil {
+                print("Facebook login failed. Error \(facebookError)")
+            } else if facebookResult.isCancelled {
+                print("Facebook login was cancelled.")
+            } else {
+                let accessToken = FBSDKAccessToken.currentAccessToken().tokenString
+                FirebaseController.base.authWithOAuthProvider("facebook", token: accessToken,
+                    withCompletionBlock: { error, authData in
+                        if error != nil {
+                            print("Login failed. \(error)")
+                        } else {
+                            print("Logged in! \(authData)")
+                        }
+                })
+            }
+        })
+    }
+    
+    
+    
+    
+    
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
 }
