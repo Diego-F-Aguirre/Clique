0bb762fecb571619971b8064b126ad9f38197379
Diego-F-Aguirre <diego3884@gmail.com>
2016-03-06 20:47:23 -0700

    Cocoa Pods installed with Firebase & JSQMessenger
    Also new LoginViewController and ChatViewController added

---
+++ FirebaseController.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseController.swift
+//  Clique
+//
+//  Created by Diego Aguirre on 3/6/16.
+//  Copyright © 2016 Diego Aguirre. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class FirebaseController {
+ 
+    static let base = Firebase(url: "https://cliqueus.firebaseio.com")
+    
+    
+}
